bba9fcf236430074e72c6ed178c08a8944cb0b1c
Juacas <juacas.diaz@gmail.com>
2024-05-26 23:01:52 -0500

    Configuraciones para docker

--- CclInventoryApp/obj/Debug/net8.0/CclInventoryApp.AssemblyInfo.cs
+++ CclInventoryApp/obj/Debug/net8.0/CclInventoryApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CclInventoryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3693359c8704db63588619db33d24a0ff0109306")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f443e3c0889cde037c0a7efd044bbb181bbe06")]
 [assembly: System.Reflection.AssemblyProductAttribute("CclInventoryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CclInventoryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
